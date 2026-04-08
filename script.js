9c6d15ffadc0b96f7e552345e0de64126ccdb0fe
Guilherme Duarte <gduartedasilva3@gmail.com>
2026-04-08 18:05:32 -0300

    .

--- script.js
+++ script.js
@@ -1,1 +1,53 @@
 let num =  document.getElementById('num')
+
+function del() {
+    num.innerHTML = ''
+}
+
+function zero() {
+    num.innerHTML += '0'
+}
+
+function um() {
+    num.innerHTML += '1'
+}
+
+function dois() {
+    num.innerHTML += '2'
+}
+
+function tres() {
+    num.innerHTML += '3'
+}
+
+function quatro() {
+    num.innerHTML += '4'
+}
+
+function cinco() {
+    num.innerHTML += '5'
+}
+
+function seis() {
+    num.innerHTML += '6'
+}
+
+function sete() {
+    num.innerHTML += '7'
+}
+
+function oito() {
+    num.innerHTML += '8'
+}
+
+function nove() {
+    num.innerHTML += '9'
+}
+
+function mais() {
+    num.innerHTML += '+'
+}
+
+function menos() {
+    num.innerHTML += '-'
+}
